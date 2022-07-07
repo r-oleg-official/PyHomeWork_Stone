a0f4646ec0edd137dd6318e2949efec5781a78cf
STONE17th <17th@inbox.ru>
2022-07-07 15:13:46 +0300

    Исправлены неточности первого ДЗ

--- HomeWork01/Task004.py
+++ HomeWork01/Task004.py
@@ -1,15 +1,14 @@
-print(" 1 | 2 \n---+---\n 4 | 3 ")
+print(" 2 | 1 \n---+---\n 3 | 4 ")
 
 num_plane = int(input("Введите номер плоскости: "))
-if num_plane in range (1,5) :
-    match num_plane:
-        case 1:
-            print(f"В {num_plane} плоскости X = [0,-inf) и Y = [0,+inf)")
-        case 2:
-            print(f"В {num_plane} плоскости X = [0,+inf) и Y = [0,+inf)")
-        case 3:
-            print(f"В {num_plane} плоскости X = [0,+inf) и Y = [0,-inf)")
-        case 4:
-            print(f"В {num_plane} плоскости X = [0,-inf) и Y = [0,-inf)")
-else:
-    print("Такой плоскости не существует")
+match num_plane:
+    case 1:
+        print(f"В {num_plane} плоскости X = [0,+inf) и Y = [0,+inf)")
+    case 2:
+        print(f"В {num_plane} плоскости X = [0,-inf) и Y = [0,+inf)")
+    case 3:
+        print(f"В {num_plane} плоскости X = [0,-inf) и Y = [0,-inf)")
+    case 4:
+        print(f"В {num_plane} плоскости X = [0,+inf) и Y = [0,-inf)")
+    case num_plane:
+        print("Такой плоскости не существует")
